660fe4e3ab28e02f25d8bc68f7b77d9d65dd9a36
Karthik Senthil <karthik.senthil@gmail.com>
2021-12-17 04:30:24 +0000

    Updated footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'helpdeskxyz';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
@@ -23,7 +23,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`made with <3 by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
