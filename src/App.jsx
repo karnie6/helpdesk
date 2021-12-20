66449f3b4b4d1dc68eb9778b0e03f70aeed325ac
Karthik Senthil <karthik.senthil@gmail.com>
2021-12-20 03:42:44 +0000

    removed buildspace image from metadata

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 import Form from 'react-bootstrap/Form'
 
-
 // Constants
 const TWITTER_HANDLE = 'helpdeskxyz';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
@@ -69,7 +68,6 @@ const App = () => {
     
       setWalletAddress(response.publicKey.toString());
     }
-
   };
 
   /*
